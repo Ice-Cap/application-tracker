8061be79d39baf42f9710683a2422f736226ad6c
Ice-Cap <66086363+Ice-Cap@users.noreply.github.com>
2024-07-10 12:03:33 -0600

    update analytics to use filters instead of additional queries

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -10,25 +10,32 @@ class AnalyticsController extends Controller
 {
     public function index(Request $request): View
     {
-        $total = DB::table('applications')->count();
-        $totalFull = DB::table('applications')
-            ->where('application_type', 'full')
-            ->count();
-        $totalEasy = DB::table('applications')
-            ->where('application_type', 'easy')
-            ->count();
-        $totalInterviews = DB::table('applications')
-            ->where('response', 'interview')
-            ->count();
-        $totalOffers = DB::table('applications')
-            ->where('response', 'job offer')
-            ->count();
-        $totalContacted = DB::table('applications')
-            ->where('contacted', 'true')
-            ->count();
-        $totalCoverLetters = DB::table('applications')
-            ->where('cover_letter', 'true')
-            ->count();
+        $totalCollection = DB::table('applications')->get();
+        $total = $totalCollection->count();
+
+        $totalFull = $totalCollection->filter(function (object $value, int $key) {
+            return $value->application_type === 'full';
+        })->count();
+
+        $totalEasy = $totalCollection->filter(function (object $value, int $key) {
+            return $value->application_type === 'easy';
+        })->count();
+
+        $totalInterviews = $totalCollection->filter(function (object $value, int $key) {
+            return $value->response === 'interview';
+        })->count();
+
+        $totalOffers = $totalCollection->filter(function (object $value, int $key) {
+            return $value->response === 'job offer';
+        })->count();
+
+        $totalContacted = $totalCollection->filter(function (object $value, int $key) {
+            return $value->contacted === true;
+        })->count();
+
+        $totalCoverLetters = $totalCollection->filter(function (object $value, int $key) {
+            return $value->cover_letter === true;
+        })->count();
 
         return view('analytics', [
             'total' => $total,
